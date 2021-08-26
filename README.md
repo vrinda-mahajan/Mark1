7dbb414b0e67115337e841f6fbd9a6ef5b0e9f6a
Vrinda Mahajan <vrindamahajan96@gmail.com>
2021-08-26 17:47:18 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This is a CLI app.
+It is asking you some questions and for every correct answer your score will increase by 1.
---
+++ index.js
@@ -0,0 +1,50 @@
+var quesList = [
+  (quesOne = {
+    question: "Where do i live? ",
+    answer: "Udhampur",
+  }),
+  (quesTwo = {
+    question: "What is my favourite food? ",
+    answer: "Momo",
+  }),
+  (quesThree = {
+    question: "What is my favourite color? ",
+    answer: "Pink",
+  }),
+];
+var highScores = [
+  {
+    name: "Ruby",
+    score: 3,
+  },
+  {
+    name: "Diya",
+    score: 2,
+  },
+];
+
+var readlineSync = require("readline-sync");
+var score = 0;
+var username = readlineSync.question("What is your name? ");
+console.log("Welcome", username, "to DO YOU KNOW Vrinda?");
+function check(ques, ans) {
+  var userAnswer = readlineSync.question(ques);
+  if (userAnswer.toUpperCase() == ans.toUpperCase()) {
+    console.log("Right!");
+    score++;
+  } else {
+    console.log("Wrong!");
+  }
+  console.log("Current Score: ", score);
+  console.log("--------------");
+}
+for (var i = 0; i < quesList.length; i++) {
+  check(quesList[i].question, quesList[i].answer);
+}
+console.log("YAY! Your Score is: ", score);
+console.log(
+  " Check Out the high scores,if you should be there ping me and i will update it."
+);
+for (var i = 0; i < highScores.length; i++) {
+  console.log(highScores[i].name, ":", highScores[i].score);
+}
